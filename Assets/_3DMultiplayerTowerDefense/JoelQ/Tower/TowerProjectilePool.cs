fd01ee1946aeda8b8a79ca60060dcc1b64f11dce
Goomer <42564474+ddark1990@users.noreply.github.com>
2019-10-23 04:21:47 -0700

    commit

--- Assets/_3DMultiplayerTowerDefense/JoelQ/Tower/TowerProjectilePool.cs
+++
@@ -1,8 +0,0 @@
-﻿#pragma warning disable CS0649
-using UnityEngine;
-namespace JoelQ.GameSystem.Tower {
-    [System.Serializable]
-    public class TowerProjectilePool : GenericPool<TowerProjectile> {
-        public TowerProjectile TowerProjectile => prefab;
-    }
-}
