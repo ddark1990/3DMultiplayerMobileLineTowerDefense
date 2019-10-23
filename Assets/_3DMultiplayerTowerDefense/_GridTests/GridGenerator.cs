6e3129f6b3b4e48aa83aae1499510fa492f80ffa
Goomer <42564474+ddark1990@users.noreply.github.com>
2019-10-23 04:06:19 -0700

    Added a grid generator
    Working on testing the new Node 2.0 system with a new generator and new way to draw nodes on screen

---
+++ Assets/_3DMultiplayerTowerDefense/_GridTests/GridGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    public Vector2 GridWorldSize;
+    public GameObject Node;
+    public float BuildTime;
+
+    int gridSizeX, gridSizeY;
+
+    public void StartGeneration()
+    {
+        StartCoroutine(CreateGrid());
+    }
+
+    public IEnumerator CreateGrid()
+    {
+        gridSizeX = Mathf.RoundToInt(GridWorldSize.x / 1);
+        gridSizeY = Mathf.RoundToInt(GridWorldSize.y / 1);
+
+        Vector3 worldBottomLeft = transform.position - Vector3.right * GridWorldSize.x / 2 - Vector3.forward * GridWorldSize.y / 2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                yield return new WaitForSeconds(BuildTime);
+
+                var node = Instantiate(Node, worldBottomLeft + new Vector3(x + 0.5f, 0, y + 0.5f), Quaternion.identity);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(GridWorldSize.x, 0, GridWorldSize.y));
+    }
+}
---
+++ Assets/_3DMultiplayerTowerDefense/_GridTests/GridNode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridNode
+{
+    public Vector3 WorldPosition;
+
+    public GridNode(Vector3 worldPos)
+    {
+        WorldPosition = worldPos;
+    }
+}
